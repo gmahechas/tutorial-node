9251b54c8f2a73264bc0313b748bd3a454c8d011
Gustavo Mahecha <gmahechas@outlook.com>
2024-02-01 23:32:23 -0500

    node

--- clean-architecture/rest/src/presentation/express/helpers/error-handler.ts
+++ clean-architecture/rest/src/presentation/express/helpers/error-handler.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
 
 export const errorHandler = (error: any, request: Request, response: Response, next: NextFunction) => {
-	response.status(500).json({ success: false, message: 'Internal Server Error', error: { code: 500, error: error.message } })
+	response.status(500).json({ success: false, message: 'Internal Server Error', error: { code: 500 } })
 };
