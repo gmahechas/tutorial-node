f5acccc6988bcab856da8a78c25b0c1c2077aeb7
Gustavo Mahecha <gmahechas@outlook.com>
2024-02-01 23:17:46 -0500

    node

--- clean-architecture/rest/src/presentation/express/post/post.controller.ts
+++ clean-architecture/rest/src/presentation/express/post/post.controller.ts
@@ -17,55 +17,55 @@ export class PostController {
 		}
 
 		new GetAllPostUseCase(this.postRepository).execute(getAllPostDto)
-			.then((posts) => {
-				response.status(200).json({ success: true, message: 'Posts fetched successfully', data: posts });
-			})
-			.catch((error) => {
-				response.status(500).json({ error });
-			});
+			.then((posts) => response.status(200).json({ success: true, message: 'Posts fetched successfully', data: posts }))
+			.catch((error) => response.status(500).json({ success: false, message: 'Internal Server Error', error: { code: 500 } }));
 	}
 
-	public getById = async (request: Request, response: Response): Promise<void> => {
+	public getById = (request: Request, response: Response): void => {
 		const [errors, getByIdPostDto] = GetByIdPostDto.create({ postId: +request.params.postId });
 		if (errors) {
 			response.status(400).json({ success: false, message: 'Bad Request', error: { code: 400, errors } });
 			return;
 		}
 
-		const post = await new GetByIdPostUseCase(this.postRepository).execute(getByIdPostDto);
-		response.status(200).json({ post });
+		new GetByIdPostUseCase(this.postRepository).execute(getByIdPostDto)
+			.then((post) => response.status(200).json({ success: true, message: 'Post fetched successfully', data: post }))
+			.catch((error) => response.status(500).json({ success: false, message: 'Internal Server Error', error: { code: 500 } }));
 	}
 
-	public create = async (request: Request, response: Response): Promise<void> => {
+	public create = (request: Request, response: Response): void => {
 		const [errors, createPostDto] = CreatePostDto.create(request.body);
 		if (errors) {
 			response.status(400).json({ success: false, message: 'Bad Request', error: { code: 400, errors } });
 			return;
 		}
 
-		const post = await new CreatePostUseCase(this.postRepository).execute(createPostDto);
-		response.status(200).json({ post });
+		new CreatePostUseCase(this.postRepository).execute(createPostDto)
+			.then((post) => response.status(200).json({ success: true, message: 'Post created successfully', data: post }))
+			.catch((error) => response.status(500).json({ success: false, message: 'Internal Server Error', error: { code: 500 } }));
 	}
 
-	public update = async (request: Request, response: Response): Promise<void> => {
+	public update = (request: Request, response: Response): void => {
 		const [errors, updatePostDto] = UpdatePostDto.create({ ...request.body, postId: +request.params.postId });
 		if (errors) {
 			response.status(400).json({ success: false, message: 'Bad Request', error: { code: 400, errors } });
 			return;
 		}
 
-		const post = await new UpdatePostUseCase(this.postRepository).execute(updatePostDto);
-		response.status(200).json({ post });
+		new UpdatePostUseCase(this.postRepository).execute(updatePostDto)
+			.then((post) => response.status(200).json({ success: true, message: 'Post updated successfully', data: post }))
+			.catch((error) => response.status(500).json({ success: false, message: 'Internal Server Error', error: { code: 500 } }));
 	}
 
-	public delete = async (request: Request, response: Response): Promise<void> => {
+	public delete = (request: Request, response: Response): void => {
 		const [errors, deletePostDto] = DeletePostDto.create({ postId: +request.params.postId });
 		if (errors) {
 			response.status(400).json({ success: false, message: 'Bad Request', error: { code: 400, errors } });
 			return;
 		}
 
-		const post = await new DeletePostUseCase(this.postRepository).execute(deletePostDto);
-		response.status(200).json({ post });
+		new DeletePostUseCase(this.postRepository).execute(deletePostDto)
+			.then((post) => response.status(200).json({ success: true, message: 'Post deleted successfully', data: post }))
+			.catch((error) => response.status(500).json({ success: false, message: 'Internal Server Error', error: { code: 500 } }));;
 	}
 }
